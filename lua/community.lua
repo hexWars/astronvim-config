9fb76180589ca5cd27f37a09fc702b383b1cd0ad
Senhsucht <tech@sehnsucht.top>
2024-06-25 17:57:10 +0800

    feat: 增加hop

--- lua/community.lua
+++ lua/community.lua
@@ -11,5 +11,6 @@ return {
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.colorscheme.tokyodark-nvim" },
   { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.motion.hop-nvim" },
   -- import/override with your plugins folder
 }
